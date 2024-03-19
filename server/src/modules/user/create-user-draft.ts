f1bcd9d104afd42ef5c07f656084ba5de63ce30c
imnites <nitesh110897@gmail.com>
2024-03-19 19:14:10 +0530

    fix

--- server/src/modules/user/create-user-draft.ts
+++ server/src/modules/user/create-user-draft.ts
@@ -1,5 +1,6 @@
 import { Context } from '~/types';
 import { MutationCreateUserDraftArgs } from '~/generated-types';
+import { GraphQLError } from 'graphql';
 
 export const createUserDraft = async (
   parent: { [key: string]: unknown } | null,
@@ -9,5 +10,27 @@ export const createUserDraft = async (
   const id = crypto.randomUUID();
   await context.redisClient.saveData(id, { ...args.input, id });
 
-  return id;
+  const [userDetailsByEmail, userDetailsByPhoneNumber] = await Promise.all([
+    context.serviceClients.userService.getUserDetailsByEmail(args.input.email),
+    context.serviceClients.userService.getUserDetailsByPhoneNumber(
+      args.input.phoneNumber
+    )
+  ]);
+
+  if (userDetailsByEmail) {
+    throw new GraphQLError('Email already exists', {
+      extensions: {
+        code: 'EMAIL_ALREADY_EXISTS'
+      }
+    });
+  } else if (userDetailsByPhoneNumber) {
+    throw new GraphQLError('Phone number already exists', {
+      extensions: {
+        code: 'PHONE_NUMBER_ALREADY_EXISTS'
+      }
+    });
+  } else {
+    await context.redisClient.saveData(id, { ...args.input, id });
+    return id;
+  }
 };
